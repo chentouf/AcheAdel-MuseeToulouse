1b675be74cd7226281a71acb8ff06bd82ec1e3cb
Anass <mychentouf@gmail.com>
2015-04-27 03:22:42 +0200

    ajouter teste pour la classe musee

--- test/unit/museetoulouse/MuseeSpec.groovy
+++ test/unit/museetoulouse/MuseeSpec.groovy
@@ -2,6 +2,7 @@ package museetoulouse
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,44 @@ import spock.lang.Specification
 @TestFor(Musee)
 class MuseeSpec extends Specification {
 
-    def setup() {
+    @Unroll
+    void "test la validite d'un musée valide"(String unNom, String unHoraireOuverture, String unTelephone, String unAccesMetro, String unAccesBus, Gestionnaire unGestionnaire, Adresse uneAdresse) {
+
+        given: "un musée initialisé avec un nom, un horaire d'ouverture, un téléphone non vides, un accès métro et un accès bus"
+        Musee musee = new Musee(nom: unNom, horairesOuverture: unHoraireOuverture, telephone: unTelephone, accesMetro: unAccesMetro, accesBus: unAccesBus, gestionnaire: unGestionnaire, adresse: uneAdresse)
+
+        expect: "le musée est valide"
+        musee.validate() == true
+
+        and: "il n'a pas de demande de visite"
+        !musee.demandevisites
+
+        where:
+        unNom       |   unHoraireOuverture          |   unTelephone         |   unAccesMetro        |   unAccesBus      |   unGestionnaire      |   uneAdresse
+        "un nom"    |   "un horaire d'ouverture"    |   "un téléphone"      |   "un accès métro"    |   "un accès bus"  |   Mock(Gestionnaire)  |   Mock(Adresse)
+        "un nom"    |   "un horaire d'ouverture"    |   "un téléphone"      |   ""                  |   ""              |   Mock(Gestionnaire)  |   Mock(Adresse)
+        "un nom"    |   "un horaire d'ouverture"    |   "un téléphone"      |   null                |   null            |   Mock(Gestionnaire)  |   Mock(Adresse)
+
     }
 
-    def cleanup() {
-    }
+    @Unroll
+    void "test l'invalidite d'un musée non valide"(String unNom, String unHoraireOuverture, String unTelephone, Gestionnaire unGestionnaire, Adresse uneAdresse) {
 
-    void "test something"() {
+        given: "une activite initialise avec un nom vide ou un horaire d'ouverture vide ou un téléphone vide ou sans gestionnaire ou sans adresse"
+        Musee musee = new Musee()
+
+        expect: "l'activite est invalide"
+        musee.validate() == false
+
+        where:
+
+        unNom       |   unHoraireOuverture          |   unTelephone     |   unGestionnaire      |   uneAdresse
+        ""          |   "un horaire d'ouverture"    |   "un téléphone"  |   Mock(Gestionnaire)  |   Mock(Adresse)
+        "un nom"    |   ""                          |   "un téléphone"  |   Mock(Gestionnaire)  |   Mock(Adresse)
+        "un nom"    |   "un horaire d'ouverture"    |   ""              |   Mock(Gestionnaire)  |   Mock(Adresse)
+        "un nom"    |   "un horaire d'ouverture"    |   "un téléphone"  |   null                |   Mock(Adresse)
+        "un nom"    |   "un horaire d'ouverture"    |   "un téléphone"  |   Mock(Gestionnaire)  |   null
+
+
     }
 }
